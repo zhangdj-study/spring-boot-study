2300959002a974cbc52cf8a9f90871eec5eb49d6
zhangdj <zhangdj_sr@neusiri.com>
2020-06-05 14:28:04 +0800

    20200605

--- src/main/java/com/zdj/config/RedisProperties.java
+++ src/main/java/com/zdj/config/RedisProperties.java
@@ -2,11 +2,14 @@ package com.zdj.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  * @author zhangdj
  * @date 2019/11/16
+ * @ConfigurationProperties  将类中的属性和配置文件的值进行绑定
  */
+@Component
 @ConfigurationProperties(prefix = "redis.config")
 @Data
 public class RedisProperties {
--- src/main/java/com/zdj/controller/HelloWorldController.java
+++ src/main/java/com/zdj/controller/HelloWorldController.java
@@ -1,8 +1,16 @@
 package com.zdj.controller;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 
 /**
  * @author zhangdj
@@ -18,4 +26,25 @@ public class HelloWorldController {
     }
 
 
+    @GetMapping
+    @RequestMapping("test")
+    public void download(HttpServletResponse response) throws Exception{
+        response.setContentType("application/octet-stream");
+        String fileName = "是的阿萨德.pdf";
+//        response.setHeader("Content-Disposition",
+//                "inline;fileName=\"" + fileName);
+        response.setHeader("Content-Disposition",
+                "inline;fileName=\"" + new String(fileName.getBytes("UTF-8"), "ISO8859-1") + "\"");
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        Document document = new Document();
+        PdfWriter writer = PdfWriter.getInstance(document, bos);
+        document.open();
+        //写"拜访对象信息"标题并且画线
+        Paragraph paragraph = new Paragraph("拜访对象信息");
+        document.add(paragraph);
+        document.close();
+        OutputStream outputStream = response.getOutputStream();
+        bos.writeTo(outputStream);
+        outputStream.flush();
+    }
 }
